6e645ecb11553e63b98ec4128475764eb0cb755b
Alex Nichol <unixpickle@gmail.com>
2018-10-14 11:52:45 -0400

    use proper CSV format
    Some names have ",JR" in them.

--- scrape/scrape.go
+++ scrape/scrape.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
+	"os"
 	"time"
 
 	"github.com/unixpickle/essentials"
@@ -10,16 +12,19 @@ import (
 const NumYears = 10
 
 func main() {
-	fmt.Println("app1,app2,date,id")
+	writer := csv.NewWriter(os.Stdout)
+	writer.Write([]string{"app1", "app2", "date", "id"})
+	writer.Flush()
 	date := time.Now()
 	for i := 0; i < 365*NumYears; i++ {
 		dateStr := fmt.Sprintf("%02d/%02d/%04d", date.Month(), date.Day(), date.Year())
 		results, err := MarriagesAtDate(dateStr)
 		essentials.Must(err)
 		for _, result := range results {
-			fmt.Printf("%s,%s,%s,%s\n", result.Applicant1, result.Applicant2, result.Date,
-				result.LicenseID)
+			writer.Write([]string{result.Applicant1, result.Applicant2, result.Date,
+				result.LicenseID})
 		}
+		writer.Flush()
 		date = date.AddDate(0, 0, -1)
 	}
 }
